1d7f5b668960646f1c114fe8c798f62880cd5d2b
syedbasha138 <syedbasha2227@gmail.com>
2024-10-13 13:43:46 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "mybucketforpracticejenkinsserver"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "lockforstatefile"
     encrypt        = true
   }
 }
